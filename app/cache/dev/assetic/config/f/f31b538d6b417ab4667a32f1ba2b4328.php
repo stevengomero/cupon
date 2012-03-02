f59a142b0158f8ddc24efea16ad5aef7397e868a
Steven <estebanarmas@gmail.com>
2012-03-02 14:24:47 +0000

    arregladas las tildes de la página de ayuda.

---
+++ app/cache/dev/assetic/config/f/f31b538d6b417ab4667a32f1ba2b4328.php
@@ -0,0 +1,5 @@
+<?php
+
+// OfertaBundle:Default:ayuda.html.twig
+return array (
+);
--- app/cache/dev/twig/f3/dd/4e99d15311d9cda2531b047d23e4.php
+++ app/cache/dev/twig/f3/dd/4e99d15311d9cda2531b047d23e4.php
@@ -6,7 +6,16 @@ class __TwigTemplate_f3dd4e99d15311d9cda2531b047d23e4 extends Twig_Template
     protected function doDisplay(array $context, array $blocks = array())
     {
         // line 1
-        echo "Esto será la página de ayuda de la web.
+        echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\">
+<head>
+<meta http-equiv=\"Content-Type\" content=\"text/html; charset= ISO-8859-1\" />
+<title>Titulo del documento</title>
+</head>
+<body>
+Esto ser&aacute la p&aacutegina de ayuda de la web.
+</body>
+</html>
 ";
     }
 
