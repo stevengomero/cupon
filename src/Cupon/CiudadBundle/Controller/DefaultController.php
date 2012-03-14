8639ec261b788724f0947b89180a4e4e9fd61b0c
Steven <estebanarmas@gmail.com>
2012-03-14 11:58:56 +0000

    ultimos avances en el proyecto. Incluidos los css, y las plantillas twig con herencia.

--- src/Cupon/CiudadBundle/Controller/DefaultController.php
+++ src/Cupon/CiudadBundle/Controller/DefaultController.php
@@ -3,13 +3,22 @@
 namespace Cupon\CiudadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
+class DefaultController extends Controller {
 
-class DefaultController extends Controller
-{
-    
-    public function indexAction($name)
-    {
-        return $this->render('CiudadBundle:Default:index.html.twig', array('name' => $name));
+    public function cambiarAction($ciudad) {
+	return new RedirectResponse($this->generateUrl('portada', array('ciudad' => $ciudad)));
     }
+
+    public function listaCiudadesAction($ciudad) {
+	$em = $this->getDoctrine()->getEntityManager();
+	$ciudades = $em->getRepository('CiudadBundle:Ciudad')->findAll();
+	return $this->render('CiudadBundle:Default:listaCiudades.html.twig', 
+		array(	'ciudades' => $ciudades,
+			'ciudadActual' => $ciudad
+		)
+	);
+    }
+
 }
